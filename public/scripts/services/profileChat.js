4b1fcb2fd609b18ccb8672bd62a9a71f253a0d81
hardi <hardik.munjaal@gmail.com>
2016-01-05 19:12:01 +0530

    test connection with server for socket io and send username from client to server from angular

---
+++ public/scripts/services/profileChat.js
@@ -0,0 +1,24 @@
+loginApp.factory('socket', function ($rootScope) {
+  //var socket = io.connect();
+  var socket = io('http://localhost:5000');
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
