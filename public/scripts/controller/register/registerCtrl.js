ccbf6182446ade0c48ece3fbd0d4ce1586063009
hardi <hardik.munjaal@gmail.com>
2016-03-02 18:31:06 +0530

    add email and mobile existence checker

--- public/scripts/controller/register/registerCtrl.js
+++ public/scripts/controller/register/registerCtrl.js
@@ -1,16 +1,18 @@
 loginApp.controller('registerController', function($scope,RegisterService,$location,$rootScope) {
         // create a message to display in our view
 
+        $scope.emailExisterMessage='';
+        $scope.mobileExisterMessage='';
+
         RegisterService.list().success(function(data) {
         	$scope.users=data;
         });
         $scope.form_data={};
         $scope.message = 'api call to be made on click of register';
+
         $scope.register = function() {
-        	//console.log($scope.vm.user);
         	RegisterService.registerUser($scope.form_data).success(function(data) {
         		$scope.notification=data;
-//should do these things only on success notification
                 if (confirm("User details are registered successfully!  Now press OK to continue") == true) {
                     $rootScope.session = true;
                     $rootScope.noSession = false;
@@ -22,6 +24,34 @@ loginApp.controller('registerController', function($scope,RegisterService,$locat
              $rootScope.noSession = false;
              $location.path( "/zoozoo" );
          });
+        };
+
+        $scope.emailExistenceChecker = function() {
+
+           RegisterService.emailExistenceChecker($scope.form_data.email).success(function(data) {
+                
+                if (data[0].total>0) {
+                   $scope.emailExisterMessage='Sorry Bro Email Id is already registered';
+                } else {
+                 $scope.emailExisterMessage='Email Id is available';
+             }
+             
+         });
+
+        };
+
+        $scope.mobileExistenceChecker = function() {
+
+           RegisterService.mobileExistenceChecker($scope.form_data.mobile).success(function(data) {
+                
+                if (data[0].total>0) {
+                   $scope.mobileExisterMessage='Sorry Bro MObile No. is already registered';
+                } else {
+                 $scope.mobileExisterMessage='Mobile No. is available';
+             }
+             
+         });
+
         };
 
     });
--- public/scripts/services/registerSvc.js
+++ public/scripts/services/registerSvc.js
@@ -8,6 +8,18 @@ loginApp.factory("RegisterService", ['$http', function($http) {
     first: function() {
       return users[0];
     },
+    emailExistenceChecker:function(params) {
+      return $http({
+        url: 'http://0.0.0.0:5000/v1/validate/email?email='+params,
+        method: 'GET'
+      })
+    },
+    mobileExistenceChecker:function(params) {
+      return $http({
+        url: 'http://0.0.0.0:5000/v1/validate/mobile?mobile='+params,
+        method: 'GET'
+      })
+    },
     registerUser:function(params) {
       return $http({
         url: 'http://0.0.0.0:5000/register/user',
